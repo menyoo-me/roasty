6324f8b2b559bf965b7668d6cd0a31d4a8368937
Vinicius Cainelli <vicainelli+github@gmail.com>
2020-10-29 20:12:50 +0100

    Vue Base
    *

---
+++ src/components/logo/RstyLogo.vue
@@ -0,0 +1,19 @@
+<template>
+  <img
+    data-test-id="menyoo_logo"
+    :src="require(`@/assets/images/logo-menyoo-${variant}.svg`)"
+    alt="Menyoo Logo"
+  />
+</template>
+
+<script>
+export default {
+  name: 'rsty-logo',
+  props: {
+    variant: {
+      type: String,
+      default: 'default',
+    },
+  },
+};
+</script>
